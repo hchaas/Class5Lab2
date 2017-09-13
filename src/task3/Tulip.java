17fbe7660227227ad436cc35793ed5b18df1e7e4
User <User@10.0.1.9>
2017-09-12 22:06:00 -0500

    completed interface

--- src/task3/Tulip.java
+++ src/task3/Tulip.java
@@ -1,6 +1,52 @@
-
 package task3;
 
 public class Tulip implements Flower {
-    
+
+    private String stem;
+    private boolean leaves;
+    private String color;
+
+    @Override
+    public void growPlant() {
+        System.out.println("Tuplip grew 4 inches.");
+    }
+
+    @Override
+    public String getStem() {
+        return stem;
+    }
+
+    @Override
+    public boolean hasLeaves() {
+        return leaves;
+    }
+
+    @Override
+    public String getColor() {
+        return color;
+    }
+
+    @Override
+    public void setStem(String stem) {
+        if (stem == null || stem.isEmpty()) {
+            throw new IllegalArgumentException("Invalid input.");
+        } else {
+            this.stem = stem;
+        }
+    }
+
+    @Override
+    public void setHasLeaves(boolean leaves) {
+        leaves = true;
+    }
+
+    @Override
+    public void setColor(String color) {
+        if (color == null || color.isEmpty()) {
+            throw new IllegalArgumentException("Inavliad input.");
+        } else {
+            this.color = color;
+        }
+    }
+
 }
