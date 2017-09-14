5de1b518e67b1371218bea4c98472e659aabba3e
User <User@10.0.1.9>
2017-09-13 21:17:08 -0500

    added report service for output in task2

---
+++ src/task2/SimpleReportService.java
@@ -0,0 +1,12 @@
+
+package task2;
+
+import task1.*;
+
+public class SimpleReportService {
+    private String msg;
+    
+    public void simpleOutput(String msg){
+        System.out.println(msg);
+    }
+}
--- src/task2/Tulip.java
+++ src/task2/Tulip.java
@@ -2,8 +2,15 @@
 package task2;
 
 public abstract class Tulip extends Flower{
+    private SimpleReportService output;
+    
+    public Tulip(){
+        output = new SimpleReportService();
+    }
+    
     @Override
     public void growPlant(){
-        System.out.println("Tulip grew 4 inches.");
+        String msg = "Tulip grew 4 inches.";
+        this.output.simpleOutput(msg);
     }
 }
